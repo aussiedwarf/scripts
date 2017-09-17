bb7d4058bde03ef6e7f6a16eb8cad4514d7bb4e8
Eden Harris <aussiedwarf@gmail.com>
2017-09-18 07:34:22 +1000

    Fixes to fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -3,34 +3,62 @@
 #zlib
 #https://zlib.net/
 #SHA-256 hash 4ff941449631ace0d4d203e3483be9dbc9da454084111f97ea0a2114e19bf066
-wget -P ../thirdparty/zlib/ https://zlib.net/zlib-1.2.11.tar.xz 
-tar xf ../thirdparty/zlib/zlib-1.2.11.tar.xz  ../thirdparty/zlib/1.2.11/
+wget -O thirdparty/zlib/zlib-1.2.11.tar.xz https://zlib.net/zlib-1.2.11.tar.xz 
+tar -xf thirdparty/zlib/zlib-1.2.11.tar.xz -C thirdparty/zlib/
+
+if false
+then
 
 #libpng
 #http://www.libpng.org/pub/png/libpng.html
 #md5 e01be057a9369183c959b793a685ad15
-wget -P ../thirdparty/libpng/ ftp://ftp-osl.osuosl.org/pub/libpng/src/libpng16/libpng-1.6.32.tar.xz
-tar xf ../thirdparty/libpng/libpng-1.6.32.xz  ../thirdparty/1.6.32
+wget -O thirdparty/libpng/libpng-1.6.32.tar.xz https://download.sourceforge.net/libpng/libpng-1.6.32.tar.xz
+tar -xf thirdparty/libpng/libpng-1.6.32.tar.xz -C thirdparty/libpng/
+
 
 #http://www.ijg.org/
-wget -P ../thirdparty/libjpeg/ http://www.ijg.org/files/jpegsrc.v9b.tar.gz
-tar xf ../thirdparty/zlib/zlib-1.2.11.tar.xz  ../thirdparty/zlib/1.2.11/
+wget -O thirdparty/libjpeg/jpegsrc.v9b.tar.gz http://www.ijg.org/files/jpegsrc.v9b.tar.gz
+tar -xf thirdparty/libjpeg/jpegsrc.v9b.tar.gz -C thirdparty/libjpeg/
+
 
 #xz
 #https://tukaani.org/xz/
-wget -P ../thirdparty/xz https://tukaani.org/xz/xz-5.2.3.tar.xz
+wget -O thirdparty/xz/xz-5.2.3.tar.xz https://tukaani.org/xz/xz-5.2.3.tar.xz
+tar -xf thirdparty/xz/xz-5.2.3.tar.xz -C thirdparty/xz/
+
 
 #libtiff
 #http://www.simplesystems.org/libtiff/
-wget -P ../thirdparty/libtiff ftp://download.osgeo.org/libtiff/tiff-4.0.8.tar.gz
+wget -O thirdparty/libtiff/tiff-4.0.8.tar.gz ftp://download.osgeo.org/libtiff/tiff-4.0.8.tar.gz
+tar -xf thirdparty/libtiff/tiff-4.0.8.tar.gz -C thirdparty/libtiff/
+
 
 #http://giflib.sourceforge.net/
+wget -O thirdparty/giflib/giflib-5.1.4.tar.bz2 https://downloads.sourceforge.net/project/giflib/giflib-5.1.4.tar.bz2
+tar -xf thirdparty/giflib/giflib-5.1.4.tar.bz2 -C thirdparty/giflib/
+
 
 #http://www.bzip.org/
+#md5: 00b516f4704d4a7cb50a1d97e6e8e15b
+wget -O thirdparty/bzip2/bzip2-1.0.6.tar.gz http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
+tar -xf thirdparty/bzip2/bzip2-1.0.6.tar.gz -C thirdparty/bzip2/
+
+
 
 #SDL2
 
 #https://freetype.org/index.html
+wget -O thirdparty/SDL/SDL2-2.0.5.tar.gz https://www.libsdl.org/release/SDL2-2.0.5.tar.gz
+tar -xf thirdparty/SDL/SDL2-2.0.5.tar.gz -C thirdparty/SDL/
+
+wget -O thirdparty/SDL/SDL2_image-2.0.1.tar.gz https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1.tar.gz
+tar -xf thirdparty/SDL/SDL2_image-2.0.1.tar.gz -C thirdparty/SDL/
+
+wget -O thirdparty/SDL/SDL2_ttf-2.0.14.tar.gz https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz
+tar -xf thirdparty/SDL/SDL2_ttf-2.0.14.tar.gz -C thirdparty/SDL/
+
+wget -O thirdparty/SDL/SDL2_net-2.0.1.tar.gz https://www.libsdl.org/projects/SDL_net/release/SDL2_net-2.0.1.tar.gz
+tar -xf thirdparty/SDL/SDL2_net-2.0.1.tar.gz -C thirdparty/SDL/
 
 #webp
 
@@ -39,3 +67,5 @@ wget -P ../thirdparty/libtiff ftp://download.osgeo.org/libtiff/tiff-4.0.8.tar.gz
 #SDL2_ttf
 
 #SDL2_net
+
+fi
--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get install -y git wget
+
+#macosx
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew install wget
