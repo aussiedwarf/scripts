bae62b8635b68e15bf50928ac5816fd5b74321eb
Eden Harris <aussiedwarf@gmail.com>
2017-12-04 08:39:48 +1100

    gl for linux install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install -y git wget autoconf autoconf-archive m4 libtool
+sudo apt-get install -y git wget autoconf autoconf-archive m4 libtool libgl1-mesa-dev
 
 #macosx
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
