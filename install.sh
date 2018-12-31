56e219b22f47bd56974ce4aba097b41ad9261221
Eden Harris <aussiedwarf@gmail.com>
2018-12-31 23:57:29 +1100

    adding nasm to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get update
 
-sudo apt-get install -y git wget make g++ build-essential autoconf autoconf-archive m4 libtool libgl1-mesa-dev cmake
+sudo apt-get install -y git wget make g++ build-essential autoconf autoconf-archive m4 libtool libgl1-mesa-dev cmake nasm
 
 #macosx
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
